0fd161f48c23f572f6352f0a9bdf52d6ccbbb81d
ruslanashaari <ruslan.ashaari@gmail.com>
2020-05-16 13:17:15 +0800

    lesson 7 components 101

---
+++ js/07-components-101.js
@@ -0,0 +1,17 @@
+Vue.component('task', {
+	template: '<li><slot></slot></li>',
+
+	data() {
+		return {
+			message: 'Foobar'
+		};
+	}
+});
+
+var app = new Vue({
+	el: '#root',
+
+	data: {
+
+	}
+})
